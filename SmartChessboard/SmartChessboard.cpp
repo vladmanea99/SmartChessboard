51f1da298cb3f7f4ac7e8977ab76cdf0287786c9
Iulia Popescu <iulia.popescu0720@gmail.com>
2021-03-22 18:39:04 +0200

    Mapped naming for pieces to integers - Iulia, Marius
    Mapped naming for pieces to integers.
    Added matrix for the pieces on the board.

--- SmartChessboard/SmartChessboard.cpp
+++ SmartChessboard/SmartChessboard.cpp
@@ -3,26 +3,39 @@
 #define CPPHTTPLIB_OPENSSL_SUPPORT
 #include <httplib.h>
 #include <vector>
+#include <map>
 
+#include <stdlib.h>
+#include <base64.h>
+#include <math.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 
 using namespace std;
 using namespace sf;
+using namespace httplib;
+using namespace cv;
 
 httplib::Server svr;
+
+map<int, string> piecesMap;
+
+int board[8][8] = { 0 };
 
 class Piece {
 private:
     int x;
     int y;
-    string name;
+    int code;
 
 public:
-    Piece(int x, int y, string name) {
-        this->x = x; this->y = y; this->name = name;
+    Piece(int x, int y, int code) {
+        this->x = x; this->y = y; this->code = code;
     }
 
-    string getName() {
-        return this->name;
+    int getCode() {
+        return this->code;
     }
 
     int getX() {
@@ -50,7 +63,8 @@ void drawBoard(RenderWindow* window) {
 
 void drawPiece(RenderWindow* window, Piece piece) {
     sf::Texture pieceTexture;
-    pieceTexture.loadFromFile("pieces/" + piece.getName() + ".png");
+    string name = piecesMap[piece.getCode()];
+    pieceTexture.loadFromFile("pieces/" + name + ".png");
 
     sf::Sprite pieceSprite;
     pieceSprite.setTexture(pieceTexture);
@@ -60,40 +74,73 @@ void drawPiece(RenderWindow* window, Piece piece) {
 }
 
 void initializePieces(vector<Piece> &pieces) {
-    pieces.push_back(Piece(0, 0, "black_rook"));
-    pieces.push_back(Piece(1, 0, "black_knight"));
-    pieces.push_back(Piece(2, 0, "black_bishop"));
-    pieces.push_back(Piece(3, 0, "black_queen"));
-    pieces.push_back(Piece(4, 0, "black_king"));
-    pieces.push_back(Piece(5, 0, "black_bishop"));
-    pieces.push_back(Piece(6, 0, "black_knight"));
-    pieces.push_back(Piece(7, 0, "black_rook"));
-    pieces.push_back(Piece(0, 1, "black_pawn"));
-    pieces.push_back(Piece(1, 1, "black_pawn"));
-    pieces.push_back(Piece(2, 1, "black_pawn"));
-    pieces.push_back(Piece(3, 1, "black_pawn"));
-    pieces.push_back(Piece(4, 1, "black_pawn"));
-    pieces.push_back(Piece(5, 1, "black_pawn"));
-    pieces.push_back(Piece(6, 1, "black_pawn"));
-    pieces.push_back(Piece(7, 1, "black_pawn"));
+    pieces.push_back(Piece(0, 0, 5));
+    pieces.push_back(Piece(1, 0, 2));
+    pieces.push_back(Piece(2, 0, 0));
+    pieces.push_back(Piece(3, 0, 4));
+    pieces.push_back(Piece(4, 0, 1));
+    pieces.push_back(Piece(5, 0, 0));
+    pieces.push_back(Piece(6, 0, 2));
+    pieces.push_back(Piece(7, 0, 5));
+    pieces.push_back(Piece(0, 1, 3));
+    pieces.push_back(Piece(1, 1, 3));
+    pieces.push_back(Piece(2, 1, 3));
+    pieces.push_back(Piece(3, 1, 3));
+    pieces.push_back(Piece(4, 1, 3));
+    pieces.push_back(Piece(5, 1, 3));
+    pieces.push_back(Piece(6, 1, 3));
+    pieces.push_back(Piece(7, 1, 3));
 
-    pieces.push_back(Piece(0, 6, "white_pawn"));
-    pieces.push_back(Piece(1, 6, "white_pawn"));
-    pieces.push_back(Piece(2, 6, "white_pawn"));
-    pieces.push_back(Piece(3, 6, "white_pawn"));
-    pieces.push_back(Piece(4, 6, "white_pawn"));
-    pieces.push_back(Piece(5, 6, "white_pawn"));
-    pieces.push_back(Piece(6, 6, "white_pawn"));
-    pieces.push_back(Piece(7, 6, "white_pawn"));
+    pieces.push_back(Piece(0, 6, 9));
+    pieces.push_back(Piece(1, 6, 9));
+    pieces.push_back(Piece(2, 6, 9));
+    pieces.push_back(Piece(3, 6, 9));
+    pieces.push_back(Piece(4, 6, 9));
+    pieces.push_back(Piece(5, 6, 9));
+    pieces.push_back(Piece(6, 6, 9));
+    pieces.push_back(Piece(7, 6, 9));
+    pieces.push_back(Piece(0, 7, 11));
+    pieces.push_back(Piece(1, 7, 8));
+    pieces.push_back(Piece(2, 7, 6));
+    pieces.push_back(Piece(3, 7, 10));
+    pieces.push_back(Piece(4, 7, 7));
+    pieces.push_back(Piece(5, 7, 6));
+    pieces.push_back(Piece(6, 7, 8));
+    pieces.push_back(Piece(7, 7, 11));
 
-    pieces.push_back(Piece(0, 7, "white_rook"));
-    pieces.push_back(Piece(1, 7, "white_knight"));
-    pieces.push_back(Piece(2, 7, "white_bishop"));
-    pieces.push_back(Piece(3, 7, "white_queen"));
-    pieces.push_back(Piece(4, 7, "white_king"));
-    pieces.push_back(Piece(5, 7, "white_bishop"));
-    pieces.push_back(Piece(6, 7, "white_knight"));
-    pieces.push_back(Piece(7, 7, "white_rook"));
+    board[0][0] = 5;
+    board[0][1] = 2;
+    board[0][2] = 0;
+    board[0][3] = 4;
+    board[0][4] = 1;
+    board[0][5] = 0;
+    board[0][6] = 2;
+    board[0][7] = 5;
+    board[1][0] = 3;
+    board[1][1] = 3;
+    board[1][2] = 3;
+    board[1][3] = 3;
+    board[1][4] = 3;
+    board[1][5] = 3;
+    board[1][6] = 3;
+    board[1][7] = 3;
+
+    board[6][0] = 9;
+    board[6][1] = 9;
+    board[6][2] = 9;
+    board[6][3] = 9;
+    board[6][4] = 9;
+    board[6][5] = 9;
+    board[6][6] = 9;
+    board[6][7] = 9;
+    board[7][0] = 11;
+    board[7][1] = 8;
+    board[7][2] = 6;
+    board[7][3] = 10;
+    board[7][4] = 7;
+    board[7][5] = 6;
+    board[7][6] = 8;
+    board[7][7] = 11;
 }
 
 void initializeBoard(RenderWindow* window, vector<Piece> &pieces) {
@@ -102,10 +149,25 @@ void initializeBoard(RenderWindow* window, vector<Piece> &pieces) {
     }
 }
 
-int main() {
+void initializeMap(map<int, string> &map) {
+    map.insert({ 0, "black_bishop" });
+    map.insert({ 1, "black_king" });
+    map.insert({ 2, "black_knight" });
+    map.insert({ 3, "black_pawn" });
+    map.insert({ 4, "black_queen" });
+    map.insert({ 5, "black_rook" });
+    map.insert({ 6, "white_bishop" });
+    map.insert({ 7, "white_king" });
+    map.insert({ 8, "white_knight" });
+    map.insert({ 9, "white_pawn" });
+    map.insert({ 10, "white_queen" });
+    map.insert({ 11, "white_rook" });
+}
 
+int main() {
     sf::RenderWindow window(sf::VideoMode(800, 800), "My window");
     vector<Piece> pieces;
+    initializeMap(piecesMap);
 
     while (window.isOpen())
     {
@@ -130,9 +192,28 @@ int main() {
         window.display();
     }
 
-    svr.Get("/hi", [](const httplib::Request&, httplib::Response& res) {
-        res.set_content("Hello World!", "text/plain");
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            cout << board[i][j] << ' ';
+        }
+
+        cout << endl;
+    }
+
+    /*svr.Get("/hi", [&](const Request& req, Response& res) {
+        string image_path = "chessboard.png";
+        Mat img = cv::imread(image_path, IMREAD_COLOR);
+        string encoded_png;
+
+        vector<uchar> buf;
+        cv::imencode(".png", img, buf);
+        auto base64_png = reinterpret_cast<const unsigned char*>(buf.data());
+        encoded_png = "data:image/jpeg;base64," + base64_encode(base64_png, buf.size());
+
+        string ceva = "<img src= " + encoded_png + ">";
+        res.set_content(ceva, "text/html");
         });
-    svr.listen("127.0.0.1", 8080);
+
+    svr.listen("127.0.0.1", 8080);*/
     return 0;
 }
